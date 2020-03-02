c2cdd61141ab913bb4c8d1ed015e8a63185f7fcd
Dave Betts <ibedave777@gmail.com>
2020-03-02 10:59:12 -0500

    argh

--- knexfile.js
+++ knexfile.js
@@ -1,50 +1,119 @@
-module.exports = {
+// module.exports = {
 
+//   development: {
+//     client: 'sqlite3',
+//     useNullAsDefault: true,
+//     connection: {
+//       filename: './data/auth.db3'
+//     },
+//     migrations: {
+//       directory: './data/migrations',
+//       tableName: 'dbmigrations'
+//     },
+//     seeds:{
+//       directory: './data/seeds'
+//     },
+//     pool:{
+//       afterCreate: (conn, done)=>{
+//         conn.run('PRAGMA foreign_keys = ON',done)
+//       }
+//     },
+
+//     production: {
+//       client: "pg",
+//       connection: process.env.DATABASE_URL,
+      
+//       migrations: {
+//         directory: "./data/migrations",
+//         tableName: "dbmigrations",
+//       }
+//     }
+//   },
+//   testing:{
+//     client: 'sqlite3',
+//     useNullAsDefault: true,
+//     connection: {
+//       filename: './data/test.db3'
+//     },
+//     migrations: {
+//       directory: './data/migrations'
+//     },
+//     seeds:{
+//       directory: './data/seeds'
+//     },
+//     pool:{
+//       afterCreate: (conn, done)=>{
+//         conn.run('PRAGMA foreign_keys = ON',done)
+//       }
+//     }
+//   }
+// };
+
+// Update with your config settings.
+module.exports = {
   development: {
     client: 'sqlite3',
-    useNullAsDefault: true,
     connection: {
       filename: './data/auth.db3'
     },
-    migrations: {
-      directory: './data/migrations',
-      tableName: 'dbmigrations'
-    },
-    seeds:{
-      directory: './data/seeds'
-    },
-    pool:{
-      afterCreate: (conn, done)=>{
-        conn.run('PRAGMA foreign_keys = ON',done)
-      }
-    },
-
-    production: {
-      client: "pg",
-      connection: process.env.DATABASE_URL,
-      
-      migrations: {
-        directory: "./data/migrations",
-        tableName: "dbmigrations",
-      }
-    }
-  },
-  testing:{
-    client: 'sqlite3',
     useNullAsDefault: true,
-    connection: {
-      filename: './data/test.db3'
-    },
     migrations: {
       directory: './data/migrations'
     },
-    seeds:{
+    seeds: {
       directory: './data/seeds'
     },
-    pool:{
-      afterCreate: (conn, done)=>{
-        conn.run('PRAGMA foreign_keys = ON',done)
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on foreign key 
       }
     }
+  },
+    testing: {
+      client: 'sqlite3',
+      connection: { filename: './database/test.db3' },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './database/migrations',
+      },
+      seeds: { directory: './database/seeds' },
+    },
+    staging: {
+      client: 'sqlite3',
+      connection: {
+        filename: './data/staging.db3',
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './data/migrations',
+      },
+      seeds: {
+        directory: './data/seeds',
+      },
+    },
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: './data/migrations'
+      },
+      seeds: {
+        directory: './data/seeds'
+      },
+    },
+    // production: {
+    //   client: 'sqlite3',
+    //   connection: {
+    //     filename: './data/production.db3',
+    //   },
+    //   useNullAsDefault: true,
+    //   migrations: {
+    //     directory: './data/migrations',
+    //   },
+    //   seeds: {
+    //     directory: './data/seeds',
+    //   },
+    // },
+    
+    
   }
-};
