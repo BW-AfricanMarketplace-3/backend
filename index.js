43cc661ad73d6d36a8c04c77b6bb856da9c4dee1
Dave Betts <ibedave777@gmail.com>
2020-03-02 12:53:17 -0500

    hosted deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require('./server')
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
