3970f575eb505d2a717e57b48a5d0a663f2d1073
Dave Betts <ibedave777@gmail.com>
2020-03-03 09:04:35 -0500

    readme created

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
     if(token){
-       jwt.verify(token,process.env.JWT_SECRET || 'PasswordHidden', (err, decodedToken)=>{
+       jwt.verify(token,process.env.JWT_SECRET || 'thisonetimeatbandcamp', (err, decodedToken)=>{
            if(err){
             res.status(401).json({ message:'Invalid Token'})
            }else{
